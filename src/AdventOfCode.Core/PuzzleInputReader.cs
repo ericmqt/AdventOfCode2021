2c78975e9a7aaf81a8471e55f8ac9eb4a66db60e
Eric Smith <33182789+ericmqt@users.noreply.github.com>
2021-12-02 12:32:47 -0500

    Add IPuzzleInputParser and related helper to PuzzleInputReader

--- src/AdventOfCode.Core/PuzzleInputReader.cs
+++ src/AdventOfCode.Core/PuzzleInputReader.cs
@@ -55,4 +55,63 @@ public static class PuzzleInputReader
             lineNumber++;
         }
     }
+
+    /// <summary>
+    /// Asynchronously parses each line of the puzzle input file using the specified parser.
+    /// </summary>
+    /// <typeparam name="T">The type of value to parse from each input line.</typeparam>
+    /// <param name="parser">Instance of input line parser.</param>
+    /// <param name="fileName">The filename of the puzzle input file.</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken"/> used for cancelling the enumeration.</param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentNullException"><paramref name="parser"/> is null.</exception>
+    /// <exception cref="ArgumentException"><paramref name="fileName"/> is null or empty.</exception>
+    /// <exception cref="FileNotFoundException"><paramref name="fileName"/> refers to a file that does not exist or is inaccessible.</exception>
+    /// <exception cref="PuzzleInputReaderException">Thrown when encountering a null or empty input line.</exception>
+    /// <exception cref="PuzzleInputParsingException">Thrown when the parser is unable to parse a value from an input line.</exception>
+    public static async IAsyncEnumerable<T> ParseLinesAsync<T>(IPuzzleInputParser<T> parser, string fileName, [EnumeratorCancellation] CancellationToken cancellationToken = default)
+    {
+        if (parser is null)
+        {
+            throw new ArgumentNullException(nameof(parser));
+        }
+
+        if (string.IsNullOrEmpty(fileName))
+        {
+            throw new ArgumentException($"'{nameof(fileName)}' cannot be null or empty.", nameof(fileName));
+        }
+
+        if (!File.Exists(fileName))
+        {
+            throw new FileNotFoundException($"File specified by '{nameof(fileName)}' does not exist or is inaccessible", fileName);
+        }
+
+        // Keep track of line number for error reporting purposes
+        int lineNumber = 1;
+
+        await foreach (var inputLine in EnumerateLinesAsync(fileName, cancellationToken))
+        {
+            if (cancellationToken.IsCancellationRequested)
+            {
+                yield break;
+            }
+
+            T parsedValue;
+
+            try { parsedValue = parser.Parse(inputLine); }
+            catch (Exception ex)
+            {
+                throw new PuzzleInputParsingException(
+                    $"Unable to parse input line {lineNumber}. See inner exception for details.",
+                    inputLine,
+                    fileName,
+                    lineNumber,
+                    ex);
+            }
+
+            yield return parsedValue;
+
+            lineNumber++;
+        }
+    }
 }
